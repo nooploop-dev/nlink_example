c7ec2a6d5a11d88ee775ddc7c705d8d4be66b73b
samuelhsu <samuel.hsu@qq.com>
2021-04-07 21:03:13 +0800

    - fix variable name

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <nlink_parser/LinktrackTagframe0.h>
 #include <ros/ros.h>
 
-void tagframe0Callback(const nlink_parser::LinktrackTagframe0 &msg) {
-  ROS_INFO("msg LinktrackTagframe0 received,systemTime: %d", msg.systemTime);
+void tagframe0Callback(const nlink_parser::LinktrackTagframe0 &msg)
+{
+  ROS_INFO("msg LinktrackTagframe0 received,systemTime: %d", msg.system_time);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   ros::init(argc, argv, "linktrack_example");
   ros::NodeHandle nh;
 
